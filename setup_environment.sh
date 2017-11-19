82df5aa4ea4a6a12c70457bf121f6951818e6b26
samsdolphin <liuxiyuan95@gmail.com>
2017-11-19 12:55:38 +0800

    update the script

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,10 +5,6 @@
 wget http://nlp.stanford.edu/software/stanford-corenlp-full-2017-06-09.zip
 unzip stanford-corenlp-full-2017-06-09.zip
 rm stanford-corenlp-full-2017-06-09.zip
-cd stanford-corenlp-full-2017-06-09
 
 # Install numpy nltk xmltodict pycorenlp
 python -m pip install --user numpy nltk xmltodict pycorenlp
-
-# Run the CoreNLP server
-java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
